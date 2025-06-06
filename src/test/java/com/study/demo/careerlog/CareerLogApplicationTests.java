dc59b22c53c15cb30ede86254ac72803fad81325
Jimyeong Kim <kjm021221@icloud.com>
2025-06-06 23:17:24 +0900

    🎉Begin a Project

---
+++ src/test/java/com/study/demo/careerlog/CareerLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.demo.careerlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
